438167c071c2e4d5cf7ff0d1765f6390ea68bcf2
Malltech <Matheusvmallmann@gmail.com>
2017-06-16 18:40:18 -0300

    Update malltech.php

--- malltech.php
+++ malltech.php
@@ -4,146 +4,44 @@
  */
 define('BOT_TOKEN', '312264482:AAFF3tzO-kXpvviO1m4BnFixKv-_RMxewig');
 define('API_URL', 'https://api.telegram.org/bot' . BOT_TOKEN . '/');
-function processaMensagem($message, $malltech) {
-    $idchat = $destino = $message['chat']['id'];
-    $iduser = $message['from']['id'];
-    $user = $message['from']['first_name'];
-    $msg = $message['text'];
-    $private = ($message['chat']['type'] == "private") ? true : false;
-    $sintaxe = preg_split("/\s+/", strtolower($msg), 2);
-    $comando = substr(str_ireplace("@cavernabot", "", $sintaxe[0]), 1);
-    $criterio = $sintaxe[1];
-    if ($comando == "perguntasf" || $comando == "faq") {
-        $mensagem = "<b>O que são ChatBots?</b>\nChatbot (ou chatterbot) é um programa de computador que tenta simular um ser humano na conversação com as pessoas. O objetivo é responder as perguntas de tal forma que as pessoas tenham a impressão de estar conversando com outra pessoa e não com um programa de computador. Após o envio de perguntas em linguagem natural, o programa consulta uma base de conhecimento e em seguida fornece uma resposta que tenta imitar o comportamento humano. (<a href='https://pt.wikipedia.org/wiki/Chatterbot'>Wikipedia</a>) ";
-    }
-    if ($comando == "link" || $comando == "linksuteis") {
-        $mensagem = "<b>LINKS ÚTEIS:</b>\nComunidade no Medium:\nhttps://medium.com/botsbrasil\nLista de bots brasileiros:\nhttp://www.botsbrasil.com.br/\nFormulário para adicionar um chatbot na lista do Bots Brasil:\nhttps://botsbrasil.typeform.com/to/Y4fLU8\nNão sabe o que é chatbot?\nhttps://medium.com/botsbrasil/o-que-%C3%A9-um-chatbot-7fa2897eac5d#.yuq1q9v0s\nSe quiser participar da comunidade do ChatBot Brasil é só entrar no grupo:\nhttps://www.facebook.com/groups/chatbotbrasil/?fref=ts \nChatbot Pernambuco:\nhttps://www.facebook.com/groups/247260785715123/\n\nCríticas e sugestões serão sempre bem vindas e faz a comunidade evoluir.\nQualquer dúvida é só falar.";
-    }
-    if ($malltech == true) {
-        $time = date("H", strtotime('-3 hours'));
-        if ($time < "12") {
-            $periodo = 'manhã';
-        } else if ($time >= "12" && $time < "18") {
-            $periodo = 'tarde';
-        } else if ($time >= "18") {
-            $periodo = 'noite';
-        }
-        $palavras = preg_split("/\s+/", $criterio);
-        $intent = array_values(preg_grep("(^piada(.)?$|^batman(.)?$|^bat-man(.)?$|^profissão(.)?$|^futebol(.)?$|^time(.)?$|^raiz(.)?$|^quadrada(.)?$|^d(o|ó)lar(.)?$)", $palavras));
-        /*
-         * PIADAS
-         */
-        if (substr($intent[0], 0, 5) == 'piada') {
-            /*
-             * PIADAS DINAMICAS 
-             * @bgastaldi
-             */
-            $return = getPage('http://aspiadas.com/randomjoke.php');
-            preg_match_all('/<p>(([^.]|.)*?)<\/p>/', str_replace("<br />", "", utf8_encode($return)), $matches);
-            echo $mensagem = (isset($matches[1][0])) ? $matches[1][0] : "Desculpe patrão {$user}, hoje não estou conseguindo contar piadas...";
-        } else if (substr(strtolower($intent[0]), 0, 5) == 'dolar' || substr($intent[0], 0, 3) == 'usd' || substr(strtolower($intent[0]), 0, 5) == 'dólar') {
-            /*
-             * COTACAO DO DOLAR
-             * @bgastaldi
-             */
-            $dolar = json_decode(getPage('http://api.promasters.net.br/cotacao/v1/valores?moedas=USD&alt=json'), true);
-            $mensagem = isset($dolar['valores']['USD']['valor']) ? "Patrão {$user}, o valor do dolar agora é R$ " . number_format($dolar['valores']['USD']['valor'], 2, ',', '.') . "." : "Desculpe patrão {$user}, ainda não li o jornal hoje!";
-        } else if (substr(strtolower($intent[0]), 0, 6) == 'batman' || substr($intent[0], 0, 7) == 'bat-man') {
-            $mensagem = "Não conheço nenhum Batman. Apenas trabalho aqui.";
-        } else if (substr(strtolower($intent[0]), 0, 4) == 'time' || substr($intent[0], 0, 7) == 'futebol') {
-            $mensagem = "Não curto futebol, mas gosto do Gotham F.C.";
-        } else if (substr(strtolower($intent[0]), 0, 4) == 'raiz' || substr($intent[1], 0, 7) == 'quadrada') {
-            $numero = array_values(preg_grep("/^[+-]?[0-9]$/", $palavras));
-            $mensagem = "A raiz quadrada de " . $numero[0] . " é " . sqrt($numero[0]);
-        } else if (substr(strtolower($intent[0]), 0, 9) == 'profissão') {
-            $mensagem = "Sou mordomo";
-        } else if (strpos(strtolower($msg), 'boa noite') !== false) {
-            if ($periodo == 'noite') {
-                $mensagem = "Boa noite, patrão {$user}.";
-            } else {
-                $mensagem = "Mas está de {$periodo}, patrão {$user}";
-            }
-        } else if (strpos(strtolower($msg), 'boa tarde') !== false) {
-            if ($periodo == 'tarde') {
-                $mensagem = "Boa tarde, patrão {$user}.";
-            } else {
-                $mensagem = "Mas está de {$periodo}, patrão {$user}";
-            }
-        } else if (strpos(strtolower($msg), 'bom dia') !== false) {
-            if ($periodo == 'manhã') {
-                $mensagem = "Bom dia, patrão {$user}.";
-            } else {
-                $mensagem = "Mas está de {$periodo}, patrão {$user}";
-            }
-        } else if (strpos(strtolower($msg), 'que horas são') !== false) {
-            $mensagem = "São " . $time . " e " . date('i') . ", patrão {$user}";
-        } else if (strpos(strtolower($msg), 'melhor bot') !== false) {
-            $mensagem = "Melhor bot? Eu.";
-        } else if ($intent[0] == '') {
-            $mensagem = "Pois não, patrão {$user}.";
-        } else if ($intent[0] != '') {
-            $mensagem = "Não entendi, patrão {$user}.";
-        }
-    }
-    $replymarkup = false;
-    if ($mensagem != "") {
-        if ($replymarkup) {
-            enviaResposta("sendMessage", array('parse_mode' => 'HTML', 'chat_id' => $destino, 'disable_web_page_preview' => true, 'text' => $mensagem, 'reply_markup' => $replymarkup));
-        } else {
-            enviaResposta("sendMessage", array('parse_mode' => 'HTML', 'chat_id' => $destino, 'disable_web_page_preview' => true, 'text' => $mensagem));
-        }
-    }
+
+function processMessage($message) {
+  // processa a mensagem recebida
+  $message_id = $message['message_id'];
+  $chat_id = $message['chat']['id'];
+  if (isset($message['text'])) {
+    
+    $text = $message['text'];//texto recebido na mensagem
+    if (strpos($text, "/start") === 0) {
+		//envia a mensagem ao usuário
+      sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => 'Olá, '. $message['from']['first_name'].
+		'! Eu sou um bot que informa o resultado do último sorteio da Mega Sena. Será que você ganhou dessa vez? Para começar, escolha qual loteria você deseja ver o resultado', 'reply_markup' => array(
+        'keyboard' => array(array('Mega-Sena', 'Quina'),array('Lotofácil','Lotomania')),
+        'one_time_keyboard' => true)));
+    } 
+  } else {
+    sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => 'Desculpe, mas só compreendo mensagens em texto'));
+  }
 }
-/*
- * GET PAGE
- */
-function getPage($url) {
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_ENCODING, "utf8");
-    curl_setopt($curl, CURLOPT_USERAGENT,'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:7.0.1) Gecko/20100101 Firefox/7.0.1');
-    $output = curl_exec($curl);
-    curl_close($curl);
-    return $output;
+function sendMessage($method, $parameters) {
+  $options = array(
+  'http' => array(
+    'method'  => 'POST',
+    'content' => json_encode($parameters),
+    'header'=>  "Content-Type: application/json\r\n" .
+                "Accept: application/json\r\n"
+    )
+);
+$context  = stream_context_create( $options );
+file_get_contents(API_URL.$method, false, $context );
 }
-/*
- * FUNCAO ENVIAR RESPOSTA
- */
-function enviaResposta($metodo, $parametros) {
-    $opcoes = array(
-        'http' => array(
-            'method' => 'POST',
-            'content' => json_encode($parametros),
-            'header' => "Content-Type: application/json\r\n" .
-            "Accept: application/json\r\n"
-        )
-    );
-    $contexto = stream_context_create($opcoes);
-    file_get_contents(API_URL . $metodo, false, $contexto);
+//obtém as atualizações do bot
+$update_response = file_get_contents(API_URL."getupdates");
+$response = json_decode($update_response, true);
+$length = count($response["result"]);
+//obtém a última atualização recebida pelo bot
+$update = $response["result"][$length-1];
+if (isset($update["message"])) {
+  processMessage($update["message"]);
 }
-/*
- * RECEBE UPDATES
- */
-$update_response = file_get_contents("php://input");
-$update = json_decode($update_response, true);
-/*
- * VERIFICA SE FOI SOLICITADO CALLBACK QUERY OU REQUISICAO NORMAL
- */
-if (isset($update['callback_query'])) {
-    processaCallbackQuery($update['callback_query']);
-}
-/*
- * ATIVANDO O BOT
- */
-$malltech = (strpos(strtolower($update['message']['text']), strtolower('malltech')) !== false) ? true : false;
-if (isset($update['message']['text']) && (substr($update['message']['text'], 0, 1) == '/' || $malltech/* substr(strtolower($update['message']['text']),0,6) == 'malltech' */)) {
-    processaMensagem($update['message'], $malltech);
-}
-if (isset($update['message']['new_chat_member'])) {
-    enviaResposta("sendMessage", array('parse_mode' => 'HTML', 'chat_id' => $update['message']['chat']['id'], 'disable_web_page_preview' => true, 'text' => "Seja bem-vindo, patrão <b>{$update['message']['new_chat_member']['first_name']}</b> sinta-se a vontade. Aqui você pode falar de assuntos relacionados a: ChatBots, PLN, IA, Facebook Messenger, Slack, Telegram...\nCríticas e sugestões serão sempre bem vindas e faz a comunidade evoluir. Qualquer dúvida é só falar."));
-}
-/*
- * MENSAGEM PARA SITE
- */
-echo "Você precisa usar o Telegram para ver este App!";
+?>
